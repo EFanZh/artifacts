9366682ff747bd51b40e83c2c8b149db6c795596
EFanZh <efanzh@gmail.com>
2023-10-16 01:04:28 +0800

    Build Category Theory for Programmers natively

--- packages/category-theory-for-programmers/build.sh
+++ packages/category-theory-for-programmers/build.sh
@@ -6,13 +6,12 @@ mkdir -p "$target_path"
 
 cd "$(dirname "$0")/source"
 
+git_version=$(git show -s --format='%h')
 SOURCE_DATE_EPOCH=$(git show -s --format='%at')
 
 export FORCE_SOURCE_DATE=1 SOURCE_DATE_EPOCH
 
-nix build \
-    --extra-experimental-features flakes \
-    --extra-experimental-features nix-command \
-    '.#ctfp'
+sed -i "s/dev/$git_version/" 'src/version.tex'
+make ctfp
 
-cp 'result/ctfp.pdf' "$target_path/Category Theory for Programmers.pdf"
+mv 'src/ctfp.pdf' "$target_path/Category Theory for Programmers.pdf"
--- packages/category-theory-for-programmers/install-dependencies.sh
+++ packages/category-theory-for-programmers/install-dependencies.sh
@@ -1,6 +1,22 @@
 #!/bin/sh -e
 
-curl -L 'https://nixos.org/nix/install' | sh
+sudo apt-get install --no-install-recommends -y \
+    cm-super \
+    fonts-linuxlibertine \
+    latexmk \
+    texlive-fonts-extra \
+    texlive-fonts-recommended \
+    texlive-plain-generic \
+    texlive-xetex
 
-echo "/nix/var/nix/profiles/default/bin" >> "$GITHUB_PATH"
-echo "$HOME/.nix-profile/bin" >> "$GITHUB_PATH"
+# Install Inconsolata LGC font.
+
+inconsolata_lgc_url='https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-1.3.0/InconsolataLGC-1.3.0.tar.xz'
+font_dir=/usr/share/fonts
+
+mkdir -p "$font_dir"
+curl -L "$inconsolata_lgc_url" | sudo tar -C "$font_dir" -xJf -
+
+# Install pygments-style-github.
+
+pip install pygments-style-github
